144e12f96cd3c8664e88c42b7aecc072c54f7de0
Sceptre <mbuguag026@gmail.com>
2022-11-16 21:10:09 +0300

    deployment

--- greatfortunes/settings.py
+++ greatfortunes/settings.py
@@ -171,4 +171,4 @@ PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
